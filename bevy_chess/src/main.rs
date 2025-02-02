62f1d6429c90c8ab72714f2392d926ece8640d81
cromas <joao_marcos_calixto@hotmail.com>
2025-02-02 13:23:28 -0300

    first commit

---
+++ bevy_chess/src/main.rs
@@ -0,0 +1,58 @@
+use bevy::{prelude::*, window::{EnabledButtons, PresentMode, WindowTheme}};
+
+mod pieces;
+use pieces::*;
+
+fn main() {
+    App::new()
+            .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: String::from("Chess"),
+                resolution: Vec2::new(800., 800.).into(),
+                resizable: false,
+                fit_canvas_to_parent: true,
+                present_mode: PresentMode::AutoVsync,
+                window_theme: Some(WindowTheme::Dark),
+                enabled_buttons: EnabledButtons {
+                    maximize: false,
+                    ..default()
+                },
+                ..default()
+            }),
+            ..default()
+        }))
+        .init_resource::<SelectedPiece>()
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(Board::new())
+        .add_systems(Startup, (setup, spawn_board, spawn_pieces))
+        .add_systems(Update, update_piece)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn spawn_board(mut commands: Commands) {
+    let tile_size = 100.0;
+    let colors = [Color::srgb(0.92, 0.93, 0.82), Color::srgb(0.46, 0.59, 0.34)];
+
+    for row in 0..8 {
+        for col in 0..8 {
+            let color = colors[(row + col) % 2];
+            let x = col as f32 * tile_size - (4.0 * tile_size) + tile_size / 2.0;
+            let y = row as f32 * tile_size - (4.0 * tile_size) + tile_size / 2.0;
+
+            commands.spawn((
+                Sprite {
+                    color,
+                    custom_size: Some(Vec2::splat(tile_size)),
+                    ..default()
+                },
+                Transform::from_xyz(x, y, 0.0),
+            ));
+        }
+    }
+}
+
+
---
+++ bevy_chess/src/pieces.rs
@@ -0,0 +1,181 @@
+use bevy::{prelude::*, utils::HashMap};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum PieceColor {
+    White,
+    Black,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum PieceType {
+    Pawn(PieceColor),
+    Knight(PieceColor),
+    Bishop(PieceColor),
+    Rock(PieceColor),
+    Queen(PieceColor),
+    King(PieceColor),
+}
+
+#[derive(Default, Resource)]
+pub struct SelectedPiece {
+    pub position: Option<(usize, usize)>,
+}
+
+#[derive(Component)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(Resource)]
+pub struct Board {
+    pub pieces: HashMap<(usize, usize), PieceType>,
+}
+
+impl Board {
+    pub fn new() -> Self {
+        let mut pieces = HashMap::new();
+
+        // adds pawns
+        for i in 0..8 {
+            pieces.insert((i, 1), PieceType::Pawn(PieceColor::White));
+            pieces.insert((i, 6), PieceType::Pawn(PieceColor::Black));
+        }
+
+        // adds rocks
+        pieces.insert((0, 0), PieceType::Rock(PieceColor::White));
+        pieces.insert((7, 0), PieceType::Rock(PieceColor::White));
+        pieces.insert((0, 7), PieceType::Rock(PieceColor::Black));
+        pieces.insert((7, 7), PieceType::Rock(PieceColor::Black));
+
+        // add knights
+        pieces.insert((1, 0), PieceType::Knight(PieceColor::White));
+        pieces.insert((6, 0), PieceType::Knight(PieceColor::White));
+        pieces.insert((1, 7), PieceType::Knight(PieceColor::Black));
+        pieces.insert((6, 7), PieceType::Knight(PieceColor::Black));
+
+        // adds bishops
+        pieces.insert((2, 0), PieceType::Bishop(PieceColor::White));
+        pieces.insert((5, 0), PieceType::Bishop(PieceColor::White));
+        pieces.insert((2, 7), PieceType::Bishop(PieceColor::Black));
+        pieces.insert((5, 7), PieceType::Bishop(PieceColor::Black));
+
+        // adds queens
+        pieces.insert((3, 0), PieceType::Queen(PieceColor::White));
+        pieces.insert((3, 7), PieceType::Queen(PieceColor::Black));
+
+        // adds kings
+        pieces.insert((4, 0), PieceType::King(PieceColor::White));
+        pieces.insert((4, 7), PieceType::King(PieceColor::Black));
+
+        Self { pieces }
+    }
+
+}
+
+pub fn spawn_pieces(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let tile_size = 100.0;
+
+    let pieces = vec![
+        ("a2", "wP.png"), ("b2", "wP.png"), ("c2", "wP.png"), ("d2", "wP.png"), 
+        ("e2", "wP.png"), ("f2", "wP.png"), ("g2", "wP.png"), ("h2", "wP.png"), // white pawns
+
+        ("a7", "bP.png"), ("b7", "bP.png"), ("c7", "bP.png"), ("d7", "bP.png"), 
+        ("e7", "bP.png"), ("f7", "bP.png"), ("g7", "bP.png"), ("h7", "bP.png"), // black pawns
+
+        ("a1", "wR.png"), ("h1", "wR.png"), // white rocks
+        ("a8", "bR.png"), ("h8", "bR.png"), // black rocks
+
+        ("b1", "wN.png"), ("g1", "wN.png"), // white knights
+        ("b8", "bN.png"), ("g8", "bN.png"), // black knights
+
+        ("c1", "wB.png"), ("f1", "wB.png"), // white bishops
+        ("c8", "bB.png"), ("f8", "bB.png"), // black bishops
+
+        ("d1", "wQ.png"), ("d8", "bQ.png"), // queens
+        ("e1", "wK.png"), ("e8", "bK.png"), // kings
+    ];
+
+    for (pos, sprite) in pieces {
+        let col = pos.chars().nth(0).unwrap() as u8 - b'a';
+        let row = pos.chars().nth(1).unwrap().to_digit(10).unwrap() - 1;
+
+        let x = col as f32 * tile_size - (4.0) * tile_size + tile_size / 2.0;
+        let y = row as f32 * tile_size - (4.0) * tile_size + tile_size / 2.0;
+
+        commands.spawn((Sprite {
+            image: asset_server.load(sprite),
+            custom_size: Some(Vec2::splat(tile_size * 0.8)),
+            ..default()
+        },
+        Transform::from_xyz(x, y, 1.0),
+        Position {
+            x: col as usize,
+            y: row as usize,
+        },
+        ));
+    }
+}
+
+pub fn update_piece(
+    buttons: Res<ButtonInput<MouseButton>>, 
+    windows: Query<&Window>, 
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    board: Res<Board>,
+    mut selected: ResMut<SelectedPiece>,
+    mut query: Query<(&mut Transform, &Position)>,
+    mut commands: Commands
+    ) 
+{
+    if buttons.just_pressed(MouseButton::Left) {
+        let window = windows.single();
+        let (camera, camera_transform) = camera_query.single();
+
+        if let Some(world_pos) = window.cursor_position()
+        .and_then(|cursor| Some(camera.viewport_to_world(camera_transform, cursor)))
+        .map(|ray| ray.unwrap().origin.truncate()) 
+        {
+            let tile_width = 100.0;
+            let x = ((world_pos.x + 4.0 * tile_width) / tile_width).floor() as usize;
+            let y = ((world_pos.y + 4.0 * tile_width) / tile_width).floor() as usize;
+
+            match selected.position {
+                None => {
+                    if board.pieces.contains_key(&(x,y)) {
+                        selected.position = Some((x,y));
+                        println!("Peça selecionada em: ({}, {})", x, y);
+                    }
+                }
+
+                Some((old_x, old_y)) => {
+                    if (x,y) != (old_x, old_y) {
+                        if let Some(piece) = board.pieces.get(&(old_x, old_y)) {
+                            commands.insert_resource(Board {
+                                pieces: board.pieces.clone(),
+                            });
+
+                            let mut new_pieces = board.pieces.clone();
+                            new_pieces.remove(&(old_x, old_y));
+                            new_pieces.insert((x, y), *piece);
+
+                            commands.insert_resource(Board {
+                                pieces: new_pieces,
+                            });
+
+                            for (mut transform, pos) in &mut query {
+                                if pos.x == old_x && pos.y == old_y {
+                                    let new_x = x as f32 * tile_width - (4.0 * tile_width) + tile_width / 2.0;
+                                    let new_y = y as f32 * tile_width - (4.0 * tile_width) + tile_width / 2.0;
+
+                                    transform.translation.x = new_x;
+                                    transform.translation.y = new_y;
+                                }
+                            }
+                        }
+                    }
+                    selected.position = None;
+                }
+            }
+        }
+    }
+}
